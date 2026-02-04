ddff6e2d92b87bf42b84d60d010eb4cd215b777e
Max K <mxkk@pm.me>
2026-02-03 21:15:20 -0500

    Implement getComputerChoice function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,8 @@
-console.log("test");
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3) + 1;
+    if (choice == 1) return "Rock";
+    else if (choice == 2) return "Paper";
+    else if (choice == 3) return "Scissors"
+}
+
+console.log(getComputerChoice());
