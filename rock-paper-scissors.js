1ea1a5e9016c9c4ceb5e0a87bbcf052ed08dc7e9
Max K <mxkk@pm.me>
2026-02-03 22:33:00 -0500

    Implement playRound function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -14,5 +14,41 @@ function getHumanChoice() {
     return choice;
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock") {
+        if (computerChoice == "rock") {
+            console.log("Rock ties rock. No points");
+        } else if (computerChoice == "paper") {
+            console.log ("Paper beats rock. You lose!");
+            computerScore += 1;
+        } else if (computerChoice = "scissors") {
+            console.log("Rock beats scissors. You Win!");
+            humanScore += 1;
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice == "rock") {
+            console.log("Paper beats rock. You Win!");
+            humanScore += 1;
+        } else if (computerChoice == "paper") {
+            console.log ("Paper ties paper. No points.");
+        } else if (computerChoice = "scissors") {
+            console.log("Scissors beats paper. You Lose!");
+            computerScore += 1;
+        }
+    } else if (humanChoice === "scissors") {
+        if (computerChoice == "rock") {
+            console.log("Rock beats scissors. You lose!");
+            computerScore += 1;
+        } else if (computerChoice == "paper") {
+            console.log ("Scissors beats paper. You Win!");
+            humanScore += 1;
+        } else if (computerChoice = "scissors") {
+            console.log("Scissors ties scissors. No points.");
+        }
+    }
+}
+
+const humanSelection = getHumanChoice().toLowerCase();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
