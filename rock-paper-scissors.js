b9ab22b4b36ac3c1fbe7aeb97af9cc96c8cd4eab
Max K <mxkk@pm.me>
2026-02-03 21:18:35 -0500

    Implement getHumanChoice function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,8 +1,14 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3) + 1;
-    if (choice == 1) return "Rock";
-    else if (choice == 2) return "Paper";
-    else if (choice == 3) return "Scissors"
+    if (choice == 1) return "rock";
+    else if (choice == 2) return "paper";
+    else if (choice == 3) return "scissors"
+}
+
+function getHumanChoice() {
+    let choice = prompt("Enter rock, scissors, or paper: ");
+    return choice;
 }
 
 console.log(getComputerChoice());
+console.log(getHumanChoice());
