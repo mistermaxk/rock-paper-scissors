92402565c8ff65afa85a2d582aae913a196a0a4d
Max K <mxkk@pm.me>
2026-02-11 12:00:32 -0500

    Add UI for round results

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -9,41 +9,6 @@ function playGame() {
     let humanScore = 0;
     let computerScore = 0;
 
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice === "rock") {
-            if (computerChoice == "rock") {
-                console.log("Rock ties rock. No points");
-            } else if (computerChoice == "paper") {
-                console.log ("Paper beats rock. You lose!");
-                computerScore += 1;
-            } else if (computerChoice = "scissors") {
-                console.log("Rock beats scissors. You Win!");
-                humanScore += 1;
-            }
-        } else if (humanChoice === "paper") {
-            if (computerChoice == "rock") {
-                console.log("Paper beats rock. You Win!");
-                humanScore += 1;
-            } else if (computerChoice == "paper") {
-                console.log ("Paper ties paper. No points.");
-            } else if (computerChoice = "scissors") {
-                console.log("Scissors beats paper. You Lose!");
-                computerScore += 1;
-            }
-        } else if (humanChoice === "scissors") {
-            if (computerChoice == "rock") {
-                console.log("Rock beats scissors. You lose!");
-                computerScore += 1;
-            } else if (computerChoice == "paper") {
-                console.log ("Scissors beats paper. You Win!");
-                humanScore += 1;
-            } else if (computerChoice = "scissors") {
-                console.log("Scissors ties scissors. No points.");
-            }
-        }
-        console.log(`Score -> Human: ${humanScore} | CPU: ${computerScore}`);
-    }
-
     // human player selection
     const btnContainer = document.createElement("div");
 
@@ -69,6 +34,47 @@ function playGame() {
     btnContainer.appendChild(btnPaper);
     btnContainer.appendChild(btnScissors);
     document.body.appendChild(btnContainer);
+
+    // display results
+    const resultsDisplay = document.createElement("div");
+    const resultsP = document.createElement("p");
+    resultsDisplay.appendChild(resultsP);
+    document.body.appendChild(resultsDisplay);
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === "rock") {
+            if (computerChoice == "rock") {
+                resultsP.textContent = "Rock ties rock. No points";
+            } else if (computerChoice == "paper") {
+                resultsP.textContent = "Paper beats rock. You lose!";
+                computerScore += 1;
+            } else if (computerChoice = "scissors") {
+                resultsP.textContent = "Rock beats scissors. You Win!";
+                humanScore += 1;
+            }
+        } else if (humanChoice === "paper") {
+            if (computerChoice == "rock") {
+                resultsP.textContent = "Paper beats rock. You Win!";
+                humanScore += 1;
+            } else if (computerChoice == "paper") {
+                resultsP.textContent = "Paper ties paper. No points.";
+            } else if (computerChoice = "scissors") {
+                resultsP.textContent = "Scissors beats paper. You Lose!";
+                computerScore += 1;
+            }
+        } else if (humanChoice === "scissors") {
+            if (computerChoice == "rock") {
+                resultsP.textContent = "Rock beats scissors. You lose!";
+                computerScore += 1;
+            } else if (computerChoice == "paper") {
+                resultsP.textContent = "Scissors beats paper. You Win!";
+                humanScore += 1;
+            } else if (computerChoice = "scissors") {
+                resultsP.textContent = "Scissors ties scissors. No points.";
+            }
+        }
+        console.log(`Score -> Human: ${humanScore} | CPU: ${computerScore}`);
+    }
 
     return [humanScore, computerScore];
 }
